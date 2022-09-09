fb9b6414abfee2e5586453639ee2dd452e97acf8
unknown <gritsunov85@gmail.com>
2022-09-09 21:48:23 +0600

    1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Введите число N: ");
+int n = Convert.ToInt16(Console.ReadLine());
+
+RecursionTotal(n);
+
+void RecursionTotal(int n){
+    if(n != 1){
+
+        Console.Write(n + ",");
+        RecursionTotal(n - 1);
+
+    }
+    else{
+
+         Console.Write(n);
+         return;
+
+    }
+
+
+}
+
